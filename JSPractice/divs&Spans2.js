952de4a7774f3e606479a38ccff4e79cd6301c25
Mr. Metelus <Mdmetelus@gmail.com>
2018-10-24 23:39:20 -0400

    "...."

---
+++ JSPractice/divs&Spans2.js
@@ -0,0 +1,1 @@
+const myParagraph = document.querySelector('.little');
---
+++ cssPractice/aboutMe1.js
@@ -0,0 +1,29 @@
+document.getElementsByClassName()
+
+document.getElementsByIdName()
+
+const className = document.getElementsByClassName('child');
+console.log(className);
+
+
+
+//css style selectors
+const ids =document.queryselector('#parent');
+const ids =document.queryselector('.child');
+
+const ids =document.queryselector('p');
+const ids =document.queryselector('parent');
+
+
+
+const class = document.queryselectorAll
+
+
+const ids = document.createElement('div');
+console.log(div);
+
+const div = document.querySelector('p');
+console.log(div);
+
+const pEL = documents.querySelector('p');
+console.log(pEL);
